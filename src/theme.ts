59bde4c6577519988c7a83c288455c4e330ea880
Lilian Lee <lilianlee0213@gmail.com>
2023-04-14 15:46:06 -0400

    set up

---
+++ src/theme.ts
@@ -0,0 +1,9 @@
+import {DefaultTheme} from 'styled-components';
+export const theme: DefaultTheme = {
+	textColor: 'white',
+	bgColor: '#2f3640',
+	accentColor: '#9c88ff',
+	cardBgColor: 'transparent',
+	boardColor: '#dadfe9',
+	cardColor: 'white',
+};
