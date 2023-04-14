fce2add11330b6526c0f928f5ad4f89909c11854
Lilian Lee <lilianlee0213@gmail.com>
2023-04-14 16:22:23 -0400

    styles and placeholders

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,63 @@
-import React from 'react';
+import {DragDropContext, Draggable, Droppable} from 'react-beautiful-dnd';
+import styled from 'styled-components';
+
+const Wrapper = styled.div`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	margin: 0 auto;
+	max-width: 480px;
+	width: 100%;
+	height: 100vh;
+`;
+const Boards = styled.div`
+	display: grid;
+	grid-template-columns: repeat(1, 1fr);
+	width: 100%;
+`;
+const Board = styled.div`
+	padding: 30px 10px 20px;
+	min-height: 200px;
+	border-radius: 5px;
+	background-color: ${(props) => props.theme.boardColor};
+`;
+const Card = styled.div`
+	padding: 10px;
+	border-radius: 5px;
+	background-color: ${(props) => props.theme.cardColor};
+	margin-bottom: 5px;
+`;
+const toDos = ['a', 'b', 'c', 'd', 'e', 'f'];
 
 function App() {
-	return null;
+	const onDragEnd = () => {};
+	return (
+		<DragDropContext onDragEnd={onDragEnd}>
+			<Wrapper>
+				<Boards>
+					<Droppable droppableId="one">
+						{(provided) => (
+							<Board ref={provided.innerRef} {...provided.droppableProps}>
+								{toDos.map((toDo, index) => (
+									<Draggable draggableId={toDo} index={index}>
+										{(provided) => (
+											<Card
+												ref={provided.innerRef}
+												{...provided.dragHandleProps}
+												{...provided.draggableProps}>
+												{toDo}
+											</Card>
+										)}
+									</Draggable>
+								))}
+								{provided.placeholder}
+							</Board>
+						)}
+					</Droppable>
+				</Boards>
+			</Wrapper>
+		</DragDropContext>
+	);
 }
 
 export default App;
--- src/theme.ts
+++ src/theme.ts
@@ -1,7 +1,7 @@
 import {DefaultTheme} from 'styled-components';
 export const theme: DefaultTheme = {
 	textColor: 'white',
-	bgColor: '#2f3640',
+	bgColor: '#3f8cf2',
 	accentColor: '#9c88ff',
 	cardBgColor: 'transparent',
 	boardColor: '#dadfe9',
